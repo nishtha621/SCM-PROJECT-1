be8ac492a5e8f42db58464b9df59518cd6f2c760
nishtha621 <nishthatandon14@gmail.com>
2023-02-24 11:32:13 +0530

    area of circle is made

---
+++ AreaOfCircle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.141
+
+int main(){
+  float radius, area;
+  printf("Enter Radius: ");
+
+  scanf("%f", &radius);
+  area = PI * radius * radius;
+
+  printf("%f\n", area);
+  return 0;
+}
